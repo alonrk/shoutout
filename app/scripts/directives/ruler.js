e74d236d2bd2caee14a91134c939fba220ec6e9f
Alon Rousso Kats <alonrk@Alons-MacBook-Pro.local>
2014-01-26 12:40:51 +0200

    ruler ++

--- app/scripts/directives/ruler.js
+++ app/scripts/directives/ruler.js
@@ -9,21 +9,36 @@ angular.module('shoutoutApp')
       	$scope.stepClass = [
       		'step', 'step', 'step', 'step'
       	];
+      	$scope.stepValue = [
+      		1, 2, 3, 4
+      	];
       	
       	switch ($state.current.name)
       	{
       		case 'message':
-      			$scope.stepClass[0] = "step current";
+      			$scope.stepClass[0] = 'step current';
       			break;
       		case 'choose-style':
-      			$scope.stepClass[1] = "step current";
+      			$scope.stepClass[1] = 'step current';
+      			$scope.stepClass[0] = 'step complete';
+      			$scope.stepValue[0] = '';
       			break;
       		case 'contacts':
-      			$scope.stepClass[2] = "step current";
+      			$scope.stepClass[2] = 'step current';
+      			$scope.stepClass[1] = 'step complete';
+      			$scope.stepClass[0] = 'step complete';
+      			$scope.stepValue[1] = '';
+      			$scope.stepValue[0] = '';
       			break;
       		case 'send':
       		case 'complete':
-      			$scope.stepClass[3] = "step current";
+      			$scope.stepClass[3] = 'step current';
+      			$scope.stepClass[2] = 'step complete';
+      			$scope.stepClass[1] = 'step complete';
+      			$scope.stepClass[0] = 'step complete';
+      			$scope.stepValue[2] = '';
+      			$scope.stepValue[1] = '';
+      			$scope.stepValue[0] = '';
       			break;
       	}
       	
